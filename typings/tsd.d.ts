e64de10f9ca88d29e15cb46ccf7bb6da2288566a
Inaki Anduaga <inaki.anduaga@tado.com>
2015-11-03 23:19:53 +0100

    Removed unused gulp tsd definition

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,7 +2,6 @@
 /// <reference path="cookie-parser/cookie-parser.d.ts" />
 /// <reference path="debug/debug.d.ts" />
 /// <reference path="express/express.d.ts" />
-/// <reference path="gulp/gulp.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
 /// <reference path="morgan/morgan.d.ts" />
 /// <reference path="node/node.d.ts" />
